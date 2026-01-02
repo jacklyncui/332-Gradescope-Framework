cf7a17326c7eb308f7e99789a8decc7acb039943
Jacklyn Cui <chaozhe@cs.washington.edu>
2026-01-02 12:51:56 -0800

    fix: append the debug output rather than rewriting

--- src/edu/washington/cse332/autograder/Debug.java
+++ src/edu/washington/cse332/autograder/Debug.java
@@ -4,7 +4,7 @@ import java.io.PrintStream;
 
 public class Debug {
     public static void addDebugLine(String line) {
-        try(PrintStream debugStream = new PrintStream("debugInfo.txt")) {
+        try(PrintStream debugStream = new PrintStream(new FileOutputStream("debugInfo.txt", true))) {
             debugStream.println(line);
         } catch (Exception ignored) {
         }
